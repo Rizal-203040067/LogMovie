5b4fad3deff944ee7f56848cc70d0ca43be9387c
Rizal-203040067 <rizal.baihaqi007@gmail.com>
2023-12-08 22:14:06 +0700

    menginstal tailwind

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="css/style.css">
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <nav>
+        <ul class="topnav">
+            <li><a class="active" href="#home">Home</a></li>
+            <li><a href="#news">News</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li class="right"><a href="#about">About</a></li>
+        </ul>
+    </nav>
+
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
+</body>
+</html>
